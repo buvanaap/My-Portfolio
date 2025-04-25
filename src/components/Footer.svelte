68b35b90bc63c50993cd71a43ba1cd04f3110fed
buvanaap <128403845+buvanaap@users.noreply.github.com>
2025-04-25 22:40:11 +0530

    Add files via upload

---
+++ src/components/Footer.svelte
@@ -0,0 +1,23 @@
+<footer class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950
+flex flex-col gap-4 sm:gap-8 justify-content items-center">
+
+<p class="px-4 py-2 bg-white text-slate-950 font-medium">
+    Connect with me &rarr;
+</p>
+<div class="flex flex-col gap-4 items-center justify-center">
+    <p><b class="pr-2"> Email</b>csmbuvanaadharsini@gmail.com</p>
+    <p><b class="pr-2"> Github</b><a href="https://github.com/buvanaap" target="_blank">buvanaap
+    <sup>
+        <span class="text-xs scale-75 pl-0.5">
+            <i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"/>
+        </span>
+    </sup></a></p>
+    <p><b class="pr-2">Linkedin</b><a href="https://www.linkedin.com/in/buvanaadharsini25/" target="_blank">buvanaap
+        <sup>
+            <span class="text-xs scale-75 pl-0.5">
+                <i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"/>
+            </span>
+        </sup></a></p>
+</div>
+
+</footer>
