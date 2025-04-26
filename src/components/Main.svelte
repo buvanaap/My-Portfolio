cd3fb1846a682ff85c0911f6d5ac1b8ebdfa0e04
buvanaap <128403845+buvanaap@users.noreply.github.com>
2025-04-26 10:07:21 +0530

    Update Main.svelte

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -27,9 +27,9 @@
             <h4 class="relative z-9">Get in Touch &rarr;</h4>
         </a>
        </div>
-       <div class="relative shadow-2xl grid place-item-center">
+      <!-- <div class="relative shadow-2xl grid place-item-center">
         <img src={"images/profile.png.jpeg"} alt="Profile image" class="object-cover z-[2] max-h-[70vh]"/>
-       </div>
+       </div> --!>
 
    </section>
    <section id="projects" class="py-20 lg:py-32 flex flex-col gap-24">
